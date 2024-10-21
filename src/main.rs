5cf4f44871d0e71b81965db004bee318b7ab397c
Jioey <yifanzhu233@gmail.com>
2024-10-21 00:22:04 -0400

    feat: main algo (mostly)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-// Rust is Data Oriented?! (Contrary to OOP)
-const N:u32 = 100;
-const N_V:u32 = N * N;
-const SEED:&str = "seed";
+use rand::Rng;
 
-fn has_edge(v1:u32, v2:u32) -> bool {
+// Rust is Data Oriented?! (Contrary to OOP)
+const N:usize = 100; // Number of partitions
+const N_V:usize = N * N; // Number of Verticies
+const SEED:&str = "seed"; // Seed used in md5 hashing
+
+fn has_edge(v1:usize, v2:usize) -> bool {
     // The input string you want to hash
     let input = format!("{}{}{}", SEED, v1, v2);
 
@@ -18,12 +20,68 @@ fn has_edge(v1:u32, v2:u32) -> bool {
     return hash_as_int % 2 == 0;
 }
 
-fn worker() {
-    
+fn worker(part:Vec<usize>) -> Vec<usize>{
+    // 好像都不用adj list了？？
+    let mut to_remove:Vec<usize> = Vec::new(); // List of nodes to remove
+
+    // TODO: Create adj list    
+
+    // TODO: Sort graph? -- Genius! This essentially ensure largest degrees matches first
+
+    for v1 in part.iter() {
+        for v2 in part.iter() {
+            // Have to do lengthy .contains() because we cannot remove node half way through iterating the list
+            if v1 != v2 && 
+            !to_remove.contains(v1) &&
+            !to_remove.contains(v2) &&
+            has_edge(*v1, *v2) {
+                // Match found!
+                to_remove.push(*v1);
+                to_remove.push(*v2);
+            }
+        }
+    }
+
+    return to_remove;
 }
 
 fn main() {
     println!("Starting with N={} workers and N_V={} verticies; seed = {}", N, N_V, SEED);
+    let mut partitions:Vec<Vec<usize>> = vec![Vec::new();N]; // creates 2d vector
+    let mut removed:Vec<usize> = Vec::new();
 
-    println!("{}", has_edge(2, 3));
+    // Assign nodes
+    for node in 0..N_V {
+        let machine = rand::thread_rng().gen_range(0..N);
+        partitions[machine].push(node);
+    }
+
+    // Run workers
+    for i in 0..N {
+        // runs worker and adds to_remove (from worker) to removed (global)
+        removed.append(&mut worker(partitions[i].clone()));
+    }
+
+    // Count degrees (both before and after)
+    let mut degrees_init:Vec<usize> = vec![0;N];
+    let mut degrees_after:Vec<usize> = Vec::new();
+    for v1 in 0..N_V { // for every pair
+        for v2 in 0..N_V {
+            if v1 != v2 &&
+            has_edge(v1, v2) { // if edge exists
+                degrees_init[v1] += 1; // increment initial
+                if removed.contains(&v1) &&
+                !removed.contains(&v2) { // if node not removed
+                    degrees_after[v1] += 1; // then also increment this
+                }
+            }   
+        }
+    }
+
+
+
+
+    
+
+    println!("{}", partitions[2][0]);
 }
