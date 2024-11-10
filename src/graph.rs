20061fb65179f22f0aeacc7cc68d29bb58e8c0c4
Jioey <yifanzhu233@gmail.com>
2024-11-10 01:20:29 -0500

    feat: celeberty model for prob matrix

--- src/graph.rs
+++ src/graph.rs
@@ -1,5 +1,6 @@
-use rand::prelude::*;
+// use rand::prelude::*;
 use rand_seeder::Seeder;
+use plotly::{common::Title, layout::{Axis, BarMode}, Bar, Layout, Plot};
 
 pub struct StochasticGraph {
     n:usize,
@@ -41,35 +42,48 @@ impl StochasticGraph {
         }
     }
 
-    /// Generates a n by n probability matrix
+    /// Generates a n by n probability matrix, where n is the number of groups
     /// Values are randomly chosen from the supported probabilities using the seed
     /// Note: Only upper half (including diagonal) is populate for efficiency sake
     fn generate_prob_matrix(n:usize, seed:&str) -> Vec<Vec<f32>> {
         // Supported probabilities in prob_to_modded()
-        let supported_probs_low: Vec<f32> = vec![0.1, 0.2]; // other supported p: 0.25, 0.33
-        let supported_probs_high: Vec<f32> = vec![0.5, 1.0];
+        // let supported_probs_low: Vec<f32> = vec![0.1, 0.2]; // other p supported: 0.25, 0.33
+        // let supported_probs_high: Vec<f32> = vec![0.5, 1.0];
         
-        let num_high_degree_groups = n / 3;
+        // let num_high_degree_groups = n / 3;
 
         // Generation
         let mut prob_matrix: Vec<Vec<f32>> = vec![vec![0.0; n]; n];
 
-        let mut rng:SmallRng = Seeder::from(seed).make_rng();
+        // let mut rng:SmallRng = Seeder::from(seed).make_rng();
         
         // Assign first 1/3 groups with high degree probabilities
-        for i in 0..num_high_degree_groups {
+        // for i in 0..num_high_degree_groups {
+        //     for j in i..n {
+        //         let p = supported_probs_high.choose(&mut rng).unwrap();
+        //         prob_matrix[i][j] = *p; // only populating upper half (including diagonal)
+        //     }
+        // }
+        
+        // // Assign remaining nodes with low degree probabilities 
+        // for i in num_high_degree_groups..n {
+        //     for j in i..n {
+        //         let p = supported_probs_low.choose(&mut rng).unwrap();
+        //         prob_matrix[i][j] = *p; 
+        //     }
+        // }
+
+        // Assign remaining nodes with low degree probabilities 
+        for i in 0..n {
             for j in i..n {
-                let p = supported_probs_high.choose(&mut rng).unwrap();
-                prob_matrix[i][j] = *p; // only populating upper half (including diagonal)
+                prob_matrix[i][j] = 0.1; 
             }
         }
-        
-        // Assign remaining nodes with low degree probabilities 
-        for i in num_high_degree_groups..n {
-            for j in i..n {
-                let p = supported_probs_low.choose(&mut rng).unwrap();
-                prob_matrix[i][j] = *p; 
-            }
+
+        // Overwrites first few as high probabilities
+        let num_celebs: usize = n / 10;
+        for i in 0 ..num_celebs {
+            prob_matrix[0][i] = 0.5;
         }
 
         prob_matrix
@@ -135,5 +149,84 @@ impl StochasticGraph {
         }
 
         degrees
+    }
+
+    pub fn graph(&mut self, degrees:Vec<usize>, file_name:&str) {
+        // Graph!!
+        // Manually count distributions
+        // create bins
+        let mut bins:Vec<usize> = Vec::new();
+        let mut bin:usize = self.n_v / 2;
+        while bin >= 1 {
+            bins.push(bin);
+            bin = bin / 2;
+        }
+        
+        println!("[#Analysis#] Counting distribution...");
+        let bin_size = bins.len();
+        let mut count_init:Vec<usize> = vec![0; bin_size+1];
+        // let mut count_after:Vec<usize> = vec![0; bin_size+1];
+        for i in 0..self.n_v {
+            // count initial by bins
+            if degrees[i] == 0 {
+                count_init[bin_size] += 1;
+            } else {
+                for j in 0..bin_size {
+                    if degrees[i] > bins[j] {
+                        count_init[j] += 1;
+                        break;
+                    }
+                }
+            }
+        }
+
+        //     // count resulting by bins (this feels so inefficient....)
+        //     if degrees_after[i] == 0 {
+        //         count_after[bin_size] += 1;
+        //     } else {
+        //         for j in 0..bin_size {
+        //             if degrees_after[i] > bins[j] {
+        //                 count_after[j] += 1;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
+
+        println!("[#Analysis#] Graphing to {}...", file_name);
+        // convert bin numbers to axis ticks
+        let mut bin_labels:Vec<String> = Vec::new();
+        for b in bins {
+            let mut l = ">".to_owned();
+            l.push_str(&b.to_string());
+            bin_labels.push(l);
+        }
+        bin_labels.push("0".to_owned());
+        println!("Bin labels: {:?}", bin_labels);
+        // adding initial distribution
+        let trace1 = Bar::new(bin_labels.clone(), count_init)
+            .name("Before")
+            .opacity(0.75);
+
+        // // adding resulting distribution
+        // let trace2 = Bar::new(bin_labels.clone(), count_after)
+        //     .name("After")
+        //     .opacity(0.75);
+
+        // configuring layout
+        let layout = Layout::new()
+            .title(Title::with_text("Title"))
+            .x_axis(Axis::new().title(Title::with_text("Degrees")))
+            .y_axis(Axis::new().title(Title::with_text("Count")))
+            .bar_mode(BarMode::Overlay)
+            .bar_gap(0.05)
+            .bar_group_gap(0.2);
+        
+        // plots graph (to html file, -> open in browser)
+        let mut plot = Plot::new();
+        plot.set_layout(layout);
+        plot.add_trace(trace1);
+        // plot.add_trace(trace2);
+        plot.write_html(file_name);
     }
 }
